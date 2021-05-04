b4aea6cb6a90b94af10a4b18fd1d8f647cb52e03
roverflow <63404819+roverflow@users.noreply.github.com>
2021-05-04 16:57:49 +0530

    Update index.tsx

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -207,7 +207,7 @@ const Hero = () => {
         {/* <MailingListSignup width={width} /> */}
         <div 
           className="apply-button" 
-          data-hackathon-slug="jithack - slug" 
+          data-hackathon-slug="jithack" 
           data-button-theme="dark"
           style={{height: "44px", width: "312px"}}
         ></div>
